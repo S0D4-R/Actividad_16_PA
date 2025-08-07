aa3eca233e1df9b693e112c2517db21533b3166f
Y.V <straysheep753@gmail.com>
2025-08-07 09:49:42 -0600

    2. Lista y menú definidos

--- Actividad_16.py
+++ Actividad_16.py
@@ -1,3 +1,5 @@
+books_compendium = []
+
 class Book:
     def __init__(self, name, author, year):
         self.name = name
@@ -5,3 +7,27 @@ class Book:
         self.year = year
     def book_display(self):
         print(f"Libro: {self.name}|Autor: {self.author}|Año de publicación: {self.year}")
+
+key = True
+while key:
+    try:
+        print("----------Menú-----------")
+        ops = input("1.Agregar libros\n"
+                    "2.Mostrar la lista de libros\n"
+                    "3.Eliminar libros por título\n"
+                    "4.Salir del programa.\n"
+                    "Opción: ")
+        match ops:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                key = False
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+
+    finally:
+        print("Gracias por usar el prgrama")
